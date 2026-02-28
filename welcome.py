c64712f17fe946474d69badf18601b2e4edc60b1
nithyamovva-svg <nithyamovva@gmail.com>
2026-02-28 14:57:41 -0500

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,14 @@
+import keyword 
+print (keyword.kwlist)
+
+print ("welcome to the world of programing")
+print(46)
+print(.78)
+print (46-90)
+x=15
+y=.7
+z="cat"
+v=True
+print(x,y,z,v)
+s=input("whats your name? ")
+print(s)
